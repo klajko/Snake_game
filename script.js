f7017768a96d81ceaa40061d38a3cd4bc38bbec7
PatrikRakovicky <patrik.rako13@gmail.com>
2024-01-03 21:48:26 +0100

    aplle

--- script.js
+++ script.js
@@ -5,23 +5,35 @@ const Game_cooldown_timer = document.getElementById("cooldown_timer");
 
 //define variables of game
 const gridSize = 20;
-le
+let delay = 400;
+let snake = [{ x: 10, y: 10 }];
+createGameElement('div', 'snake');
+
+//drawing all things
+function Draw(){
+    Game_board.innerHTML = '';
+    Draw_snake()
+}
 
 function Move_snake(){
 }
 
 function Draw_snake(){
-
 }
 
-function Draw(){
-Draw_snake()
+function Draw_food(){
+    if (true){
+        const Food_element = createGameElement('div', 'apple');
+        setPosition(Food_element, food);
+        Game_board.appendChild(Food_element);
+    }
 }
 
 function Generate_food(){
-
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return { x, y };
 }
 
 function Food_snake_colision(){
-
 }
