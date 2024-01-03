716c1df05c768756e14a697874b41c06641a1717
klajko <luki.kleinert@gmail.com>
2024-01-03 20:35:20 +0100

    javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+//define basic elements of HTML
+const Game_board = document.getElementById("game_board");
+const Game_score = document.getElementById("score");
+const Game_cooldown_timer = document.getElementById("cooldown_timer");
+
+//define variables of game
+const gridSize = 20;
+le
+
+function Move_snake(){
+}
+
+function Draw_snake(){
+
+}
+
+function Draw(){
+Draw_snake()
+}
+
+function Generate_food(){
+
+}
+
+function Food_snake_colision(){
+
+}
